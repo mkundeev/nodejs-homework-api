8687e434c75e171db66c756aba5bb443378298e9
Maksym Kundeev <mkundeev@gmail.com>
2022-09-17 21:01:37 +0300

    second check

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const app = require("./app");
 require("dotenv").config();
 const { dbConnection } = require("./src/db/conection");
 
-app.listen(4000, async () => {
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, async () => {
   try {
     await dbConnection();
     console.log("Database connection successful");
-    console.log("Server running. Use our API on port: 4000");
+    console.log(`Server running. Use our API on port: ${PORT}`);
   } catch (err) {
     console.log(err.message);
     process.exit(1);
