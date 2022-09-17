78caf3022a0e7bcf213315cda2f8f80cb4691aa6
Maksym Kundeev <mkundeev@gmail.com>
2022-09-18 00:04:27 +0300

    port

--- src/models/users.js
+++ src/models/users.js
@@ -31,8 +31,8 @@ const addUser = async (body) => {
     to: body.email,
     from: "mkundeev@gmail.com",
     subject: "Contactsbook email verification",
-    text: `Please confirm your email POST http://localhost:4000/api/users/verify/${verificationToken}`,
-    html: `Please confirm your email POST http://localhost:4000/api/users/verify/${verificationToken}`,
+    text: `Please confirm your email POST ${process.env.PORT}/api/users/verify/${verificationToken}`,
+    html: `Please confirm your email POST ${process.env.PORT}/api/users/verify/${verificationToken}`,
   };
   await sgMail.send(msg);
 
