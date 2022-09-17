1462ab1b6d77efb2ba398113b9d89869226ba4d7
Maksym Kundeev <mkundeev@gmail.com>
2022-09-17 23:25:34 +0300

    1

---
+++ src/db/contactsSchema.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+  },
+});
+
+const Contact = mongoose.model("contacts", contactSchema);
+
+module.exports = {
+  Contact,
+};
