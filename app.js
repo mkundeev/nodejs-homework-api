4ea60e30b03905c4b79208b2b9178fa99b8a0be6
Maksym Kundeev <mkundeev@gmail.com>
2022-09-19 18:23:42 +0300

    swagger

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const contactsRouter = require("./src/routes/api/contacts");
 const usersRouter = require("./src/routes/api/users");
 
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./src/services/openapi.json");
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
@@ -15,6 +17,7 @@ app.use(express.json());
 
 app.use("/contacts", contactsRouter);
 app.use("/users", usersRouter);
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
